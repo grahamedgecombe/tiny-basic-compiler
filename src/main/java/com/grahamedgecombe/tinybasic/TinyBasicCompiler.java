73c317220c22476e67579499db2fe5198cecf295
Graham Edgecombe <graham@grahamedgecombe.com>
2013-04-12 20:22:42 +0100

    Add shell script to run the compiler, assembler and linker in one go.

--- src/main/java/com/grahamedgecombe/tinybasic/TinyBasicCompiler.java
+++ src/main/java/com/grahamedgecombe/tinybasic/TinyBasicCompiler.java
@@ -1,14 +1,11 @@
 package com.grahamedgecombe.tinybasic;
 
-import com.grahamedgecombe.tinybasic.ast.Program;
 import com.grahamedgecombe.tinybasic.codegen.CodeGenerator;
 import com.grahamedgecombe.tinybasic.codegen.x86_64.X86_64CodeGenerator;
 import com.grahamedgecombe.tinybasic.parser.Parser;
-import com.grahamedgecombe.tinybasic.stackir.InstructionSequence;
 import com.grahamedgecombe.tinybasic.tokenizer.Tokenizer;
 
 import java.io.IOException;
-import java.io.OutputStreamWriter;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -18,14 +15,11 @@ public final class TinyBasicCompiler {
 
     public static void main(String[] args) throws IOException {
         Path inputPath = Paths.get(args[0]);
+        Path outputPath = Paths.get(args[1]);
         try (Tokenizer tokenizer = new Tokenizer(Files.newBufferedReader(inputPath, StandardCharsets.UTF_8))) {
             try (Parser parser = new Parser(tokenizer)) {
-                Program program = parser.parse();
-
-                InstructionSequence seq = program.compile();
-
-                try (CodeGenerator generator = new X86_64CodeGenerator(new OutputStreamWriter(System.out))) {
-                    generator.generate(seq);
+                try (CodeGenerator generator = new X86_64CodeGenerator(Files.newBufferedWriter(outputPath, StandardCharsets.UTF_8))) {
+                    generator.generate(parser.parse().compile());
                 }
             }
         }
